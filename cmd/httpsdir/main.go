32cbd427bdfb0a57decfa0e1c880c27a70f521d5
Paul Scott <paul.scott@ravelin.com>
2019-06-22 01:16:54 +0100

    First attempt at mkcert.

---
+++ cmd/httpsdir/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/icio/mkcert"
+)
+
+func main() {
+	// Flags.
+	bind := flag.String("b", "localhost:12345", "bind host:addr")
+	flag.Parse()
+
+	// Create a temporary directory for the certificate files.
+	dir, err := ioutil.TempDir("", "mkcert")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get our certificate.
+	cert, err := mkcert.Exec(
+		// Domains tells mkcert what certificate to generate.
+		mkcert.Domains("localhost"),
+		// RequireTrusted(true) tells Exec to return an error if the CA isn't
+		// in the trust stores.
+		mkcert.RequireTrusted(true),
+		// CertFile and KeyFile override the default behaviour of generating
+		// the keys in the local directory.
+		mkcert.CertFile(filepath.Join(dir, "cert.pem")),
+		mkcert.KeyFile(filepath.Join(dir, "key.pem")),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Using certificate: %#v", cert)
+	log.Printf("✨ https://%s/ ✨", *bind)
+
+	// Launch the server.
+	h := http.FileServer(http.Dir("."))
+	log.Fatal(http.ListenAndServeTLS(*bind, cert.File, cert.KeyFile, h))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/icio/mkcert
+
+go 1.12
